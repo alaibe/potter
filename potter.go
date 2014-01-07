d2b0d080e34980616d927773e2fc3e447e1cef25
Anthony Laibe <anthony@laibe.cc>
2014-01-07 14:53:22 +0100

    Some refactors and make test pass

--- potter.go
+++ potter.go
@@ -2,30 +2,30 @@ package potter
 
 import "fmt"
 
-func price(books []int) float64 {
+func Price(books []int) float64 {
   sortedBooks := organize(books)
   discount    := calc(sortedBooks)
-  
+
   return discount
 }
 
 func organize(books []int) map[int]int {
   sortedBooks := make(map[int]int)
-  
+
   for index, value := range books {
     if _, present := sortedBooks[value]; present == false {
       sortedBooks[value] = 1
     }
-    
+
     if index + 1 == len(books) {
       break
     }
-    
+
     if next := books[index+1]; value == next {
       sortedBooks[value] += 1
     }
   }
-  
+
   return sortedBooks
 }
 
@@ -33,23 +33,26 @@ func calc(sortedBooks map[int]int) float64 {
   sum := 0.0
   fmt.Println(sortedBooks)
   for len(sortedBooks) > 0 {
-        
+
     switch len(sortedBooks) {
       case 1:
         sum += 8.0
+        decrementBooks(sortedBooks)
       case 2:
         sum += 15.2
+        decrementBooks(sortedBooks)
       case 3:
         sum += 21.6
+        decrementBooks(sortedBooks)
       case 4:
         sum += 25.6
+        decrementBooks(sortedBooks)
       case 5:
-        sum += 30.0
+        sum += bestPriceFor(sortedBooks)
     }
-    
-    decrementBooks(sortedBooks)
+
   }
-  
+
   return sum
 }
 
@@ -60,7 +63,26 @@ func decrementBooks(sortedBooks map[int]int) map[int]int {
       delete(sortedBooks, key)
     }
   }
-  
+
+  return sortedBooks
+}
+
+func decrementOneBook(sortedBooks map[int]int) map[int]int {
+  numOfOne := 0
+  for key, value := range sortedBooks {
+    if value == 1 {
+      numOfOne += 1
+    }
+    if numOfOne == 2 && value == 1 {
+      continue
+    }
+
+    sortedBooks[key] -= 1
+    if value == 1 {
+      delete(sortedBooks, key)
+    }
+  }
+
   return sortedBooks
 }
 
@@ -71,9 +93,11 @@ func bestPriceFor(sortedBooks map[int]int) float64 {
       countOne += 1
     }
   }
-  if countOne >= 2 {
+  if countOne == 2 {
+    decrementOneBook(sortedBooks)
     return 25.6
   } else {
+    decrementBooks(sortedBooks)
     return 30.0
   }
 }
--- potter_test.go
+++ potter_test.go
@@ -5,39 +5,39 @@ import "testing"
 func TestBasics(t *testing.T){
   expected := float64(0)
   books := []int{}
-  result := price(books)
+  result := Price(books)
   assert_equal(expected, result, t)
 
   expected = float64(8)
   books = []int{0}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 
   expected = float64(16)
   books = []int{0, 0}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 }
 
 func TestSimpleDiscounts(t *testing.T){
   expected := 8 * 2 * 0.95
   books := []int{0, 1}
-  result := price(books)
+  result := Price(books)
   assert_equal(expected, result, t)
 
   expected = 8 * 3 * 0.9
   books = []int{0, 1, 2}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 
   expected = 8 * 4 * 0.8
   books = []int{0, 1, 2, 3}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 
   expected = 8 * 5 * 0.75
   books = []int{0, 1, 2, 3, 4}
-  result = price(books)
+  result = Price(books)
 
   assert_equal(expected, result, t)
 }
@@ -45,38 +45,38 @@ func TestSimpleDiscounts(t *testing.T){
 func TestSeveralDiscounts(t *testing.T){
   expected := 8 + (8 * 2 * 0.95)
   books := []int{0, 0, 1}
-  result := price(books)
+  result := Price(books)
   assert_equal(expected, result, t)
 
   expected = 2 * (8 * 2 * 0.95)
   books = []int{0, 0, 1, 1}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
-  
+
   expected = (8 * 4 * 0.8) + (8 * 2 * 0.95)
   books = []int{0, 0, 1, 2, 2, 3}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
-  
+
   expected = 8 + (8 * 5 * 0.75)
   books = []int{0, 1, 1, 2, 3, 4}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 }
 
 func TestEdgeCases(t *testing.T){
   expected := 2 * (8 * 4 * 0.8)
   books := []int{0, 0, 1, 1, 2, 2, 3, 4}
-  result := price(books)
+  result := Price(books)
   assert_equal(expected, result, t)
-  
+
   expected = 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
   books = []int{0, 0, 0, 0, 0,
-                 1, 1, 1, 1, 1, 
+                 1, 1, 1, 1, 1,
                  2, 2, 2, 2,
                  3, 3, 3, 3, 3,
                  4, 4, 4, 4}
-  result = price(books)
+  result = Price(books)
   assert_equal(expected, result, t)
 }
 
