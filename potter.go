6ec86eab83a743f035805d0dea7d1de5ede4de54
Anthony Laibe <anthony@laibe.cc>
2014-01-07 17:41:28 +0100

    Use class, wip

--- potter.go
+++ potter.go
@@ -2,102 +2,99 @@ package potter
 
 import "fmt"
 
-func Price(books []int) float64 {
-  sortedBooks := organize(books)
-  discount    := calc(sortedBooks)
-
-  return discount
+type Book struct {
+  name, count int
 }
 
-func organize(books []int) map[int]int {
-  sortedBooks := make(map[int]int)
+type Basket []*Book
 
-  for index, value := range books {
-    if _, present := sortedBooks[value]; present == false {
-      sortedBooks[value] = 1
+var prices = [5]float64 {8.0, 15.2, 21.6, 25.6, 30}
+
+func Price(books []int) float64 {
+  basket   := build(books)
+
+  return basket.Price()
+}
+
+func build(books []int) Basket {
+  basket := Basket{}
+
+  for _, value := range books {
+    if book, present := basket.Get(value); present == true {
+      book.count += 1
+    } else {
+      book := new(Book)
+      book.name = value
+      book.count = 1
+      basket = append(basket, book)
     }
+  }
+  return basket
+}
 
-    if index + 1 == len(books) {
+func (basket Basket) Get(name int) (*Book, bool) {
+  for _, book := range basket {
+    if book.name == name {
+      return book, true
+    }
+  }
+  return nil, false
+}
+
+func (basket Basket) countBooks() int {
+  count := 0
+  for _, book := range basket {
+    if book.count > 0 {
+      count += 1
+    }
+  }
+
+  return count
+}
+
+func (basket Basket) countUniqueBooks() int {
+  count := 0
+  for _, book := range basket {
+    if book.count == 0 {
+      count += 1
+    }
+  }
+
+  return count
+}
+
+func (basket Basket) incrementCounterWithOne() {
+  for _, book := range basket {
+    if book.count == 1 {
+      book.count += 1
       break
     }
-
-    if next := books[index+1]; value == next {
-      sortedBooks[value] += 1
-    }
   }
-
-  return sortedBooks
 }
 
-func calc(sortedBooks map[int]int) float64 {
-  sum := 0.0
-  fmt.Println(sortedBooks)
-  for len(sortedBooks) > 0 {
+func (basket Basket) Price() float64 {
+  price := 0.0
 
-    switch len(sortedBooks) {
-      case 1:
-        sum += 8.0
-        decrementBooks(sortedBooks)
-      case 2:
-        sum += 15.2
-        decrementBooks(sortedBooks)
-      case 3:
-        sum += 21.6
-        decrementBooks(sortedBooks)
-      case 4:
-        sum += 25.6
-        decrementBooks(sortedBooks)
-      case 5:
-        sum += bestPriceFor(sortedBooks)
+  for basket.countBooks() > 0 {
+    len := basket.countBooks()
+    if basket.countUniqueBooks() == 2 {
+      price += prices[len-2]
+      basket.incrementCounterWithOne()
+    } else {
+      price += prices[len-1]
     }
-
+    basket.decrementCounters()
   }
 
-  return sum
+  return price
 }
 
-func decrementBooks(sortedBooks map[int]int) map[int]int {
-  for key, value := range sortedBooks {
-    sortedBooks[key] -= 1
-    if value == 1 {
-      delete(sortedBooks, key)
-    }
+func (basket Basket) decrementCounters() {
+  for _, book := range basket {
+    book.count -= 1
   }
-
-  return sortedBooks
 }
 
-func decrementOneBook(sortedBooks map[int]int) map[int]int {
-  numOfOne := 0
-  for key, value := range sortedBooks {
-    if value == 1 {
-      numOfOne += 1
-    }
-    if numOfOne == 2 && value == 1 {
-      continue
-    }
-
-    sortedBooks[key] -= 1
-    if value == 1 {
-      delete(sortedBooks, key)
-    }
-  }
-
-  return sortedBooks
-}
-
-func bestPriceFor(sortedBooks map[int]int) float64 {
-  countOne := 0
-  for _, value := range sortedBooks {
-    if value == 1 {
-      countOne += 1
-    }
-  }
-  if countOne == 2 {
-    decrementOneBook(sortedBooks)
-    return 25.6
-  } else {
-    decrementBooks(sortedBooks)
-    return 30.0
-  }
+func bestPriceFor(sortedBooks map[int]int) {
+  fmt.Println("test")
 }
